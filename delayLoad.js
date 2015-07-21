1f783980c3e27d222595955e7ec89029a326c89d
Mr.Co <co.mr.co@gmail.com>
2015-07-21 18:46:54 +0800

    图片懒加载

---
+++ delayLoad.js
@@ -0,0 +1,20 @@
+﻿/*
+ * 图片延时加载
+ * Date:2011-06-17
+ * CodeBy:Zhongke
+ * Mail:zhongke@hichina.com
+*/
+(function ($) {
+    var delayLoad = function () {
+        $(window).scroll(function () {
+            var sTop = $(this).scrollTop() + $(this).height();
+            $('img[data-url]').each(function () {
+                if ($(this).offset().top <= sTop) {
+                    $(this).attr('src', $(this).attr('data-url')).removeAttr('data-url');
+                }
+            })
+        });
+        $(window).scroll();
+    }
+    $.delayLoad = delayLoad;
+})(jQuery)
